0719d51ed9642aac0e166524345e4806b3fd9512
Tran Vu Bao Ngoc <izhann.toyiw@gmail.com>
2025-08-04 21:48:24 +0700

    update Github Actions

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,32 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import { resolve } from 'path';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+  base: '/hanious.github.io/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        signin: resolve(__dirname, 'signin.html'),
+        signup: resolve(__dirname, 'signup.html'),
+        design: resolve(__dirname, 'design.html'),
+        template: resolve(__dirname, 'template.html'),
+      },
+    },
+  },
+});
